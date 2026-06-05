52c0169caf6acd0b3cc9e4af03ea00ede8219a4e
vishalkumar182 <vishal8049kumar@gmail.com>
2026-06-05 11:15:20 +0530

    Day 2: for loop in C

---
+++ day_2/for_loop.c
@@ -0,0 +1,18 @@
+/*#include<stdio.h>
+int main(){
+    int i;
+    for(i=0;i<=10;i++){
+        printf(" %d",i);
+        
+    }
+    return 0;
+}*/
+#include<stdio.h>
+int main(){
+    int i;
+    for(i=10;i>=0;i--){
+        printf(" %d",i);
+        
+    }
+    return 0;
+}
