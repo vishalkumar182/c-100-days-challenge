f5623d0538804901e82d27f156d0010518261446
vishalkumar182 <vishal8049kumar@gmail.com>
2026-06-05 11:27:57 +0530

    Day 2: for loop in C

--- day_2/for_loop.c
+++ day_2/for_loop.c
@@ -7,12 +7,35 @@ int main(){
     }
     return 0;
 }*/
-#include<stdio.h>
+//reverse order
+
+/*#include<stdio.h>
 int main(){
     int i;
     for(i=10;i>=0;i--){
         printf(" %d",i);
         
     }
+    return 0;
+}*/
+
+//move by 2
+/*#include<stdio.h>
+int main(){
+    int i;
+    for(i=0;i<=20;i+=2){
+        printf(" %d",i);
+        
+    }
+    return 0;
+}*/
+//sum of first 10 natural numbers
+#include<stdio.h>
+int main(){
+    int i, sum=0;
+    for(i=1;i<=10;i++){
+        sum+=i;
+    }
+    printf("Sum of first 10 natural numbers is: %d", sum);
     return 0;
 }
