d0022c4d98554e990fc512676a42f0d8224fac51
vishalkumar182 <vishal8049kumar@gmail.com>
2026-06-07 10:12:41 +0530

    Day 2: while  loop in C

--- day_2/while_loop.c
+++ day_2/while_loop.c
@@ -9,12 +9,23 @@ int main(){
     return 0;
 }*/
 //reverse order
-#include<stdio.h>
+/*#include<stdio.h>
 int main(){
     int i=10, sum=0;
     while(i>=0){
         sum+=i;
         i--;
+    }
+    printf("Sum of first 10 natural numbers is: %d", sum);
+    return 0;
+}*/
+//move by 2
+#include<stdio.h> 
+int main(){
+    int i=0, sum=0;
+    while(i<=20){
+        sum+=i;
+        i+=2;
     }
     printf("Sum of first 10 natural numbers is: %d", sum);
     return 0;
