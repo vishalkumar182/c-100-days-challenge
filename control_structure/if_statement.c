30bbb728c8de47db7b2249a6e36b4ba70a1754c4
vishalkumar182 <vishal8049kumar@gmail.com>
2026-07-03 13:10:59 +0530

    control statement

---
+++ control_structure/if_statement.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("Start small. Ship something.\n");
+    int x =10;
+    if(x==5); //scope of if statement is for one line only 
+    printf("my name is vishal kumar");      
+     return 0;
+}
