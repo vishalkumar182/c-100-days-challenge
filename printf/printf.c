f6cdaaf7fe18a183bc2882142198ea8f4e3b4201
vishalkumar182 <vishal8049kumar@gmail.com>
2026-07-07 19:08:23 +0530

    input output overvieww

---
+++ printf/printf.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main() {
+    printf("hi vishal how are you\n");//anything inside double quotes will be printed on the screen
+    int score = 100;
+printf("Score hai: %d", score); // %d acts a placeholder where we can put the value of variable
+    return 0;
+}
