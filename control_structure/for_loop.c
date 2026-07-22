96f54f548959e8a65fb7726bcf2cceba972c6311
vishalkumar182 <vishal8049kumar@gmail.com>
2026-07-22 11:20:54 +0530

    for loop  overvieww

---
+++ control_structure/for_loop.c
@@ -0,0 +1,20 @@
+/*#include <stdio.h>
+
+int main() {
+    // Basic syntax: print numbers from 1 to 5
+    for (int i = 1; i <= 5; i++) {
+        printf("Number: %d\n", i);
+    }
+    return 0;
+}*/
+#include <stdio.h>
+
+int main() {
+    printf("Counting down even numbers:\n");
+    // Start at 10, stop when i falls below 0, subtract 2 each time
+    for (int i = 10; i >= 0; i -= 2) {
+        printf("%d ", i);
+    }
+    printf("\n");
+    return 0;
+}
